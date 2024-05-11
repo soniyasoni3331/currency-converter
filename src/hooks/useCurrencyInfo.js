88ff5f98ddd8f756d9a54817f2c6b2b51188b15f
Soniya <soniyasoni3331@gmail.com>
2024-05-12 00:38:07 +0530

    first commit

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,35 @@
+// import {useState, useEffect} from "react"
+
+// function useCurrencyInfo(currency){
+//     const [data, setdata] = useState({})
+//     useEffect(()=>{
+//         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+//         .then((res) => res.json())
+//         .then((res) => setdata(res[currency]))
+//         console.log(data);
+//     },[currency])
+
+//     console.log(data);
+//     return data
+// }
+
+// export default useCurrencyInfo;
+
+
+import { useEffect, useState } from "react";
+
+import React from 'react'
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]));
+        console.log(data)
+    },[currency])
+    console.log(data)
+  return data
+}
+
+export default useCurrencyInfo;
